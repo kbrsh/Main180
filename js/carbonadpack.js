6b133a1907d36cbbb9aaafd76886c0e9ef0b722f
KingPixil <simplecooldude1@gmail.com>
2016-02-15 19:11:34 -0800

    Create carbonadpack.js

---
+++ js/carbonadpack.js
@@ -0,0 +1,42 @@
+var filename='http://www.main180.tk/css/carbonadstyles.css' + new Date().getTime();		
+var fileref=document.createElement("link");
+fileref.setAttribute("rel", "stylesheet");
+fileref.setAttribute("type", "text/css");
+fileref.setAttribute("href", filename);
+document.getElementsByTagName("head")[0].appendChild(fileref);
+
+var demoad = document.createElement('div');
+demoad.id = 'cdawrap';
+demoad.innerHTML = '<span id="cda-remove"></span>';
+document.getElementsByTagName('body')[0].appendChild(demoad);
+
+document.getElementById('cda-remove').addEventListener('click',function(e){
+	demoad.style.display = 'none';
+	e.preventDefault();
+});
+
+var bsa = document.createElement('script');
+bsa.type = 'text/javascript';
+bsa.async = true;
+bsa.id = '_carbonads_js';
+bsa.src = '//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=180iocom';
+demoad.appendChild(bsa);
+
+var adChecked = false;
+var attempts = 5;
+var cntAttempts = 0;
+var adInterval;
+
+function checkAd() {
+	if (adChecked || cntAttempts >= attempts) {
+		clearInterval(adInterval);
+		return;
+	}
+
+	cntAttempts++;
+
+	var carbonImg = document.querySelector('.carbon-img');
+
+	if (!carbonImg) return;
+
+	var adImgHeight = carbonImg.offsetHeight;
