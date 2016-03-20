0d7ae30c7259c42ca9922d52b7f7be6d83ce622c
KingPixil <simplecooldude1@gmail.com>
2016-03-19 22:04:11 -0700

    Update platform.js

--- public/js/platform.js
+++ public/js/platform.js
@@ -5,7 +5,7 @@ function getRandom(min, max) {
 Crafty.init(window.innerWidth,window.innerHeight, document.getElementById('game'));
 
 Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: 0, y: 900, w: window.innerWidth, h: 10})
+  .attr({x: 0, y: 500, w: window.innerWidth, h: 10})
   .color('green');
 
 Crafty.e('2D, Canvas, Color, Twoway, Gravity')
@@ -17,26 +17,26 @@ Crafty.e('2D, Canvas, Color, Twoway, Gravity')
 
 /* Make a ton of floors */
 Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: getRandom(50, 1000), y: getRandom(50, 1000), w: 300, h: 10})
+  .attr({x: getRandom(50, 1000), y: getRandom(50, 900), w: 300, h: 10})
   .color('green');
 
 Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: getRandom(50, 1000), y: getRandom(50, 1000), w: 300, h: 10})
+  .attr({x: getRandom(50, 1000), y: getRandom(50, 900), w: 300, h: 10})
   .color('green');
 
 Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: getRandom(50, 1000), y: getRandom(50, 1000), w: 300, h: 10})
+  .attr({x: getRandom(50, 1000), y: getRandom(50, 900), w: 300, h: 10})
   .color('green');
 
 Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: getRandom(50, 1000), y: getRandom(50, 1000), w: 300, h: 10})
+  .attr({x: getRandom(50, 1000), y: getRandom(50, 900), w: 300, h: 10})
   .color('green');
 
 Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: getRandom(50, 1000), y: getRandom(50, 1000), w: 300, h: 10})
+  .attr({x: getRandom(50, 1000), y: getRandom(50, 900), w: 300, h: 10})
   .color('green');
 
 Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: getRandom(50, 1000), y: getRandom(50, 250), w: 300, h: 10})
+  .attr({x: getRandom(50, 1000), y: getRandom(50, 900), w: 300, h: 10})
   .color('green');
 
