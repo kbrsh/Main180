c02aa65397a8a5fe0a98027d8e10c28eb750f269
KingPixil <simplecooldude1@gmail.com>
2016-04-10 19:27:18 -0700

    Update randomgradient.js

--- public/js/randomgradient.js
+++ public/js/randomgradient.js
@@ -1,13 +1,7 @@
 var bg = document.getElementById("bg");
 var out = document.getElementById("gradient");
+var output = document.querySelector(".output")
 
-document.body.onkeyup = function(e) {
-  if (e.keyCode == 32) {
-    randomBackground();
-  }
-}
-
-document.addEventListener("click", randomBackground)
 
 function randomBackground() {
   var from = '#' + Math.random().toString(16).slice(2, 8)
@@ -19,6 +13,21 @@ function randomBackground() {
 }
 
 randomBackground();
+
+function tooltip() {
+  output.setAttribute("data-balloon", "Copied To Clipboard!")
+  output.setAttribute("data-balloon-pos", "up")
+}
+
+// All Events
+document.body.onkeyup = function(e) {
+  if (e.keyCode == 32) {
+    randomBackground();
+  }
+}
+
+document.addEventListener("click", randomBackground)
+output.addEventListener("click", tooltip)
 
 // Clipboard Config
 var clipboard = new Clipboard('.output');
