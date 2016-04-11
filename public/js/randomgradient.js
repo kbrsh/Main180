6da8169a4cc9c80c7a3c956be71998d2dae34f98
KingPixil <simplecooldude1@gmail.com>
2016-04-10 19:16:27 -0700

    Create randomgradient.js

---
+++ public/js/randomgradient.js
@@ -0,0 +1,24 @@
+var bg = document.getElementById("bg");
+var out = document.getElementById("gradient");
+
+document.body.onkeyup = function(e) {
+  if (e.keyCode == 32) {
+    randomBackground();
+  }
+}
+
+document.addEventListener("click", randomBackground)
+
+function randomBackground() {
+  var from = '#' + Math.random().toString(16).slice(2, 8)
+  var to = '#' + Math.random().toString(16).slice(2, 8)
+  var deg = Math.floor(Math.random() * 100).toString();
+  var gradient = 'linear-gradient(' + deg + 'deg,' + from + ',' + to + ')';
+  bg.style.background = gradient;
+  out.innerHTML = "background:" + gradient + ";";
+}
+
+randomBackground();
+
+// Clipboard Config
+new Clipboard('.output');
