1c8c94dfda90ccdbb8dcc1f47c5f751d0687a4a9
KingPixil <simplecooldude1@gmail.com>
2016-04-20 17:33:52 -0700

    Create etch.js

---
+++ public/js/etch.js
@@ -0,0 +1,28 @@
+// The starting position of the line
+var position = new Point(100, 100);
+
+// The amount we will move when one of the keys is pressed:
+var step = 10;
+
+var path = new Path();
+path.strokeColor = 'black';
+path.add(position);
+
+function onKeyDown(event) {
+	if(event.key == 'a') {
+		position.x -= step;
+	}
+
+	if(event.key == 'd') {
+		position.x += step;
+	}
+
+	if(event.key == 'w') {
+		position.y -= step;
+	}
+
+	if(event.key == 's') {
+		position.y += step;
+	}
+	path.add(position);
+}
