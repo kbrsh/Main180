f2444e9394d2e3369a6feb2a9a8c8bc8d07478b8
KingPixil <simplecooldude1@gmail.com>
2016-04-22 19:53:13 -0700

    Update password.js

--- public/js/password.js
+++ public/js/password.js
@@ -6,6 +6,6 @@ function gen() {
   var pass = chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)]
   document.getElementById("pass").innerHTML = pass
 }
-var btn = document.getElementsByClassName("btn")
+var btn = document.getElementById("btn")
 
 btn.addEventListener("click", gen)
