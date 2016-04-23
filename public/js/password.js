217a31448f36ce8492e293cbc5bc25453a397827
KingPixil <simplecooldude1@gmail.com>
2016-04-22 19:38:54 -0700

    Create password.js

---
+++ public/js/password.js
@@ -0,0 +1,11 @@
+var charString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+"
+
+var chars = charString.split("")
+
+function gen() {
+  var pass = chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)] + chars[Math.floor(Math.random()*chars.length)]
+  document.getElementById("pass").innerHTML = pass
+}
+
+
+document.addEventListener("click", gen)
